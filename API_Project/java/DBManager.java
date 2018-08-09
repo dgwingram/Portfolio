014f4702b7b7646af3de88b5f2810b113e5f5ba0
dgwingram <41647671+dgwingram@users.noreply.github.com>
2018-08-08 22:19:48 -0400

    Added Database Manager
    Added file for API Project from SLC projects

---
+++ API_Project/java/DBManager.java
@@ -0,0 +1,43 @@
+package ca.on.sl.comp208.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by DGWIngram on 2017-03-20.
+ */
+
+public class DBManager extends SQLiteOpenHelper {
+    private final static String DBName = "demo.db";
+    private final static int VERSION = 3;
+    private Context context;
+
+    public DBManager(Context context) {
+        super(context, DBName, null, VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i("DB","onCreate");
+        String createString=
+                "create TABLE " +
+                        DBSchema.Musician.TABLE_NAME+" ( "+
+                        DBSchema.Musician._ID +" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        DBSchema.Musician.FIRST+" TEXT, " +
+                        DBSchema.Musician.LAST+" TEXT, " +
+                        DBSchema.Musician.INSTRUMENT+" TEXT);";
+        Log.i("DB","On Create: "+createString);
+        db.execSQL(createString);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String drop = "DROP TABLE "+
+                DBSchema.Musician.TABLE_NAME +";";
+        db.execSQL(drop);
+        onCreate(db);
+    }
+}
